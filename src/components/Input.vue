5489c710ead65944de1cf37cfcecac7901b76aeb
Volperter <florianellhof020205@gmail.com>
2023-11-30 15:17:52 +0100

    deletet setupProxy fix getEmission hardcoded input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -4,21 +4,21 @@
       <v-row>
         <v-col cols="12" md="3">
           <v-sheet :max-height="600" :max-width="600" :min-width="300" :min-height="530" border>
-            <v-text-field ref="RefStart"
+            <v-text-field v-model="end"
                           clearable
                           variant="solo-filled"
                           :rules="[rules.required]"
                           type="text"
                           label="Startort"
             ></v-text-field>
-            <v-text-field ref="RefEnd"
+            <v-text-field v-model="start"
                           clearable
                           variant="solo-filled"
                           :rules="[rules.required]"
                           type="text"
                           label="Zielort"
             ></v-text-field>
-            <v-select v-model="transportType"
+            <v-select v-model="transportType" @update:modelValue="resetVehicle"
                       label="Transportmethode"
                       variant="solo-filled"
                       :rules="[rules.required]"
@@ -26,7 +26,7 @@
                       item-title="name"
                       item-value="value"
             ></v-select>
-            <v-select v-model="transportVehicle" ref="RefTrans" v-if="this.transportType == 'car'"
+            <v-select v-model="transportVehicle" v-if="this.transportType == 'Car'" @update:modelValue="resetFuel"
                       clearable
                       label="Autotyp"
                       variant="solo-filled"
@@ -34,37 +34,65 @@
                       item-title="name"
                       item-value="value"
             ></v-select>
-            <v-text-field ref="Refconsumption" v-if="this.transportType == 'car'"
+            <v-text-field v-model="consumption" v-if="this.transportType == 'Car'"
                           clearable
                           variant="solo-filled"
                           type="number"
                           label="Literverbrauch"
                           suffix="L/100km"
             ></v-text-field>
-            <v-select  ref="RefFuel" v-if="this.transportType == 'car'"
+            <v-select  v-model="fuel" v-if="this.transportType == 'Car'"
                       clearable
+                       :disabled="selectDisable"
                       label="Treibstoff"
                       variant="solo-filled"
-                      :items="fuels"
+                      :items=getFuels
                       item-title="name"
                       item-value="value"
             ></v-select>
-            <v-select  ref="RefSize" v-if="this.transportType == 'car'"
+            <v-select  v-model="size" v-if="this.transportType == 'Car'"
                        clearable
                        label="Größe"
                        variant="solo-filled"
-                       :items="sizes"
+                       :items=getSizes
                        item-title="name"
                        item-value="value"
             ></v-select>
-            <v-select v-model="transportVehicle" v-if="this.transportType == 'train'"
+            <v-select v-model="transportVehicle" v-if="this.transportType == 'Train'" @update:modelValue="resetFuel"
                       label="Zugtyp"
                       variant="solo-filled"
                       :items="trains"
                       item-title="name"
                       item-value="value"
             ></v-select>
-            <v-btn type="submit">
+            <v-select  v-model="fuel" v-if="this.transportType == 'Train'"
+                       clearable
+                       :disabled="selectDisable"
+                       label="Treibstoff"
+                       variant="solo-filled"
+                       :items=getFuels
+                       item-title="name"
+                       item-value="value"
+            ></v-select>
+            <v-select v-model="transportVehicle" v-if="this.transportType == 'Bus'" @update:modelValue="resetFuel"
+                      label="Bustyp"
+                      variant="solo-filled"
+                      :items="busses"
+                      item-title="name"
+                      item-value="value"
+            ></v-select>
+            <v-select  v-model="fuel" v-if="this.transportVehicle == 'Bus_Public'"
+                       clearable
+                       :disabled="selectDisable"
+                       label="Treibstoff"
+                       variant="solo-filled"
+                       :items=getFuels
+                       item-title="name"
+                       item-value="value"
+            ></v-select>
+            <v-btn type="submit"
+                   :loading="loadingSubmit"
+              >
               Submit
             </v-btn>
 
@@ -86,11 +114,13 @@
 
                   <!-- Just for testing -->
                   <p v-if="showResult">
-                    Start: {{start}} <br />
-                    Ziel: {{end}} <br />
-                    Transportmethode: {{transportType}} <br />
-                    Transportfahrzeug: {{transportVehicle}} <br />
-                    L/100km: {{consumption}} <br />
+                    start: {{start}} <br />
+                    end: {{end}} <br />
+                    transportType: {{transportType}} <br />
+                    transportVehicle: {{transportVehicle}} <br />
+                    fuel: {{fuel}} <br />
+                    size: {{size}} <br />
+                    consumption (L/100km): {{consumption}} <br />
                   </p>
                   <!-- Just for testing -->
 
@@ -123,26 +153,32 @@
 </template>
 
 <script>
+import {watch} from "vue";
+
 export default {
   name: "Input",
 
   data(){
     return{
+      selectDisable: false,
+      loadingSubmit: false,
       size:'',
       fuel:'',
-      consumption: 0,
+      consumption: null,
       start:'',
       end:'',
       transportType:'',
       transportVehicle:'',
       showResult: false,
       co2: 0.0,
-      transport:[{value:"car", name:"Auto"}, {value:"train",name:"Zug"}, {value:"Fahrrad",name:"Fahrrad"}, {value:"foot",name:"Fuß"}, {value:"bus", name:"Bus"}],
+      transport:[{value:"Car", name:"Auto"}, {value:"Train",name:"Zug"}, {value:"Bike",name:"Fahrrad"}, {value:"Bus", name:"Bus"}],
       cars: [{value: "Pkw", name:"PKW"}],
-      busses: [{value: "BusLinie", name:"Bus"}, {value:"BusReise", name:"Reisebus"}],
-      trains: [{value: "hsb", name:"HSB"}, {value:"regional", name:"Regionalbahn"}],
+      busses: [{value: "Bus_Public", name:"Bus"}, {value:"Bus_Tour", name:"Reisebus"}],
+      trains: [{value: "Train", name:"Zug"}],
       sizes: [{value: 1, name: "Klein"}, {value: 2,name:"Mittel"},  {value: 3,name:"Gross"}],
-      fuels: [ {value: "diesel",name:"Diesel"}, {value: "petrol",name:"Benzin"}, {value: "electro",name:"elektrisch"}],
+      fuels: [ {value: "diesel",name:"Diesel", user:["Pkw", "Bus_Public", "Train"]}, {value: "petrol",name:"Benzin",user:["Pkw"]}, {value: "electric",name:"elektrisch",user:["Pkw","Train"]},
+               {value: "phev_otto",name:"Benzin Plug-In Hybrid",user:["Pkw"]}, {value: "phev_diesel",name:"Diesel Plug-In Hybrid",user:["Pkw"]} , {value: "lpg",name:"Autogas",user:["Pkw"]},
+               {value: "cng",name:"Erdgas",user:["Bus_Public"]}   ],
       rules: {
         required: value => !!value || 'Erforderlich',
       },
@@ -150,42 +186,53 @@ export default {
   },
   methods:{
     handleSubmit(){
-
-      if(this.$refs.RefStart.value == "" || this.$refs.RefEnd.value == "" || this.transportType == "")
-        return
-      this.start = this.$refs.RefStart.value
-      this.end = this.$refs.RefEnd.value
-      this.size = this.$refs.RefSize.value
-      this.fuel = this.$refs.RefFuel.value
-      if(this.$refs.Refconsumption != null)
-        this.consumption = this.$refs.Refconsumption.value
-       this.showResult = true
-      this.getEmission()
+      if(this.start == "" || this.end == "" || this.transportType == "")
+        return;
+      this.loadingSubmit = true;
+      this.getEmission();
     },
     clearRes(){
-      this.showResult = false
+      this.showResult = false;
     },
-    //TODO uses localhost, should use proxy
-    getEmission() {
-      fetch('api/emission', {
-        method: 'POST',
-        body: {
-          "startLocation": this.start,
-          "endLocation": this.end,
-          "transportMediumDTO": {
-            "transportMediumName": this.transportVehicle,
-            "transportMediumSize": this.size,
-            "transportMediumFuel": this.fuel
-          }
+    resetVehicle(){
+      this.transportVehicle = "";
+      this.resetFuel()
+      this.resetSize()
+    },
+    resetFuel(){
+      this.fuel = "";
+    },
+    resetSize(){
+      this.size = "";
+    },
+    async getEmission() {
+
+      const bodyData = {
+        "startLocation": this.start,
+        "endLocation": this.end,
+        "transportMediumDTO": {
+          "transportMediumName": this.transportVehicle,
+          "transportMediumSize": this.size,
+          "transportMediumFuel": this.fuel
         }
-      })
-      .then(res => {
+      };
+      console.log("GET EMISSION: " + JSON.stringify(bodyData));
+      await fetch('api/emission', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(bodyData)
+      }).then(res => {
         return res.json();
-      })
-      .then((data) => {
-        this.co2 = data.emission
-      })
+      }).then((data) => {
+        this.co2 = data;
+        this.showResult = true;
+        this.loadingSubmit = false;
+      });
     },
+    /*
     fetchTransport(){
       fetch('api/transport')
           .then(res => {
@@ -194,15 +241,40 @@ export default {
           .then((data) => {
             //TODO fuel values in array, size values in array (exlcude "DEFAULT"), transportMedium values in array
 
-            console.log(data)
+            console.log(data);
           })
     }
+    */
   },
+  /*
   mounted() {
-    this.fetchTransport()
+    this.fetchTransport();
   },
-  computed(){
-    //TODO make function to return custom array for fuels for each vehicle wit different fuels (if fuels is saved in one array)
+  */
+  computed: {
+    getSizes(){
+      if(this.fuel == "lpg")
+        return [ {value: 2,name:"Mittel"}];
+      if(this.fuel == "electric")
+        return [{value: 1, name: "Klein"}, {value: 2,name:"Mittel"}];
+      return this.sizes;
+    },
+    getFuels()
+    {
+      let tempFuels = [];
+      for(let i = 0; i < this.fuels.length; i++){
+        if(this.fuels[i].user.includes(this.transportVehicle))
+          tempFuels.push({value: this.fuels[i].value, name: this.fuels[i].name});
+      }
+      if(tempFuels.length == 0)
+          this.selectDisable = true;
+      else
+        this.selectDisable = false;
+      return tempFuels;
+    }
+  },
+  watch: {
+
   }
 }
 </script>
